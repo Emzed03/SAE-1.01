39881c1034323bbb2e5a90e1ce664dce9cd7d5e3
ZakyVreel <zakariya.saoula@gmail.com>
2021-10-20 16:02:29 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "test.h"
+
+void fEnregEnvoisV1(void)
+{
+	FILE *flot;
+	int ref, qte, secu;
+	float prix;
+	flot=fopen("stock.txt", "a");
+	if (flot == NULL)
+	{
+		printf("Problème d'ouverture en écriture du fichier.\n");
+		exit(1);
+	}
+	fSaisieStock(&nProd, &j, &m, &a, &nDest, &qte, &poids);
+	while (nProd!=0)
+	{
+		fprintf(flot,"%d\t %d/%d/%d\t %d\t %d\t %.1f\t \n", nProd, j, m, a, nDest, qte, poids);
+		fSaisieStock(&nProd, &j, &m, &a, &nDest, &qte, &poids);
+	}
+	fclose(flot);
+}	
