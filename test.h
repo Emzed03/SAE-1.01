8ed1e2b3e6237a880e3dc7a74ff1a0880b1fe326
Moad ZOUHRI <92849639+Emzed03@users.noreply.github.com>
2021-10-21 14:21:53 +0200

    Update test.h

--- test.h
+++ test.h
@@ -1,4 +1,9 @@
 void fEnregStock(void);
 void fSaisieStock(int *ref, int *qte, float *prix, int *secu, char *rep);
 int chargement(int ref[], int qte[], float prix[],int secu[], int tmax);
-void etatStock(int ref[], int qte[], float prix[], int secu[], int nb)
+void etatStock(int ref[], int qte[], float prix[], int secu[], int nb);
+
+
+
+
+int fApprovi(int ref[], int qte[], int secu[]
