6f0ca71cb95cc0035751240d640aed7b82bb508b
ZakyVreel <zakariya.saoula@gmail.com>
2021-10-21 14:00:10 +0200

    Update testtest.c

--- testtest.c
+++ testtest.c
@@ -3,7 +3,13 @@
 
 void global(void)
 {
+	int ref[100],qte[100], secu[100], nb;
+          float prix[100];
           fEnregStock;
+	nb=chargement(ref,qte,prix,secu,100);
+	if(nb==-1 || nb==-2)
+		return;
+	afficherTabs(num,moy,type,nb);
 }    
    
 int main(void)
